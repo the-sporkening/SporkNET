043dffd494fc29bd1c19911552b53808d59e975a
Stephen F <snipeydev@gmail.com>
2019-11-13 13:09:51 -0800

    x

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,21 @@
     </div>
     <div class="container mx-auto mt-16">
       <div class="flex justify-center">
-        <nuxt-link to="/teams">
+        <!-- <nuxt-link to="/teams">
           <div class="inline-block bg-transparent mx-4 text-orange-300 font-semibold hover:text-white py-4 px-12 border border-orange-500 hover:border-orange-900 rounded">
             Teams
           </div>
-        </nuxt-link>
+        </nuxt-link> -->
         <a href="//discord.getsporked.lol">
           <div class="inline-block bg-transparent mx-4 text-orange-300 font-semibold hover:text-white py-4 px-12 border border-orange-500 hover:border-orange-900 rounded">
             Discord
           </div>
         </a>
-        <nuxt-link to="/invite">
+        <!-- <nuxt-link to="/invite">
           <div class="inline-block bg-transparent mx-4 text-orange-300 font-semibold hover:text-white py-4 px-12 border border-orange-500 hover:border-orange-900 rounded">
             SporkBot
           </div>
-        </nuxt-link>
+        </nuxt-link> -->
       </div>
     </div>
   </div>
